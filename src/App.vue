8fb1840855c115b240d62ca297e251187cff74dc
Oscar Padilla <PadillaOscarA@gmail.com>
2023-05-05 02:11:52 -0400

    Update img-src

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@ useMeta({
   meta: [
     { name: 'robots', Name: 'robots', content: 'index,follow' },
     // CSP
-    { 'http-equiv': 'Content-Security-Policy', content: "default-src 'none'; script-src 'self' 'unsafe-eval' https://d3js.org; connect-src 'self' https://d3js.org https://gist.githubusercontent.com; img-src 'self'; style-src 'self' 'unsafe-inline' https://d3js.org; base-uri 'self'; form-action 'self'; manifest-src 'self'" },
+    { 'http-equiv': 'Content-Security-Policy', content: "default-src 'none'; script-src 'self' 'unsafe-eval' https://d3js.org; connect-src 'self' https://d3js.org https://gist.githubusercontent.com; img-src 'self' data:; style-src 'self' 'unsafe-inline' https://d3js.org; base-uri 'self'; form-action 'self'; manifest-src 'self'" },
     // OpenGraph data
     { property: 'og:title', content: metaTitle },
     { property: 'og:site_name', content: metaSiteName },
